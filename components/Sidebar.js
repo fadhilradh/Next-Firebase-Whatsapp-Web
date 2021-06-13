2989f5e16c7eb33cea77bca636316aeb6cc1bb95
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-13 10:14:29 +0700

    finalize sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,44 +1,79 @@
 import styled from 'styled-components';
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, IconButton, Button } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
   return (
     <Container>
-      <Header>
-        <IconButton>
-          <UserAvatar/>
-        </IconButton>
-      
-        <IconsContainer>
-            <IconButton>
-               <ChatIcon/>
-            </IconButton>
-            <IconButton>
-               <MoreVertIcon/>
-            </IconButton>
-        </IconsContainer>
-      </Header>
+        <Header>
+          <IconButton>
+            <UserAvatar/>
+          </IconButton>
+        
+          <IconsContainer>
+              <IconButton>
+                <ChatIcon/>
+              </IconButton>
+              <IconButton>
+                <MoreVertIcon/>
+              </IconButton>
+          </IconsContainer>
+        </Header>
+
+
+        <SearchContainer>
+            <SearchIcon fontSize="large"/>
+            <SearchInput placeholder="Search Chats Here" height={20}/>
+        </SearchContainer>
+        <StartChatButton>Start New Chat</StartChatButton>
     </Container>
   )
 }
 
 export default Sidebar
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column; 
+`;
+
 const Header = styled.div`
- display: flex;
-`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid whitesmoke;
+`;
 
 const IconsContainer = styled.div``
 
-const UserAvatar = styled(Avatar)`
+const UserAvatar = styled(Avatar)``;
 
+const SearchContainer = styled.div`
+    padding-left: 1rem;
+    display : flex;
+    align-items: center;
+    border-bottom: 1 solid whitesmoke;
 `;
 
-const Container = styled.div`
-    display: flex;
-`
+const SearchInput = styled.input`
+    outline-width: 0;
+    border: none;
+    margin: 0 1rem 0 0.5rem;
+    flex: 1;
+`;
+
+const StartChatButton = styled(Button)`
+    color: grey;
+
+    &&& {
+      border-bottom: 1px solid whitesmoke;
+      border-top: 1px solid whitesmoke;
+    };
+`;
+
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
 import Sidebar from "../components/Sidebar"
+import dynamic from 'next/dynamic'
+
+const SidebarNoSSR = dynamic(
+  () => import('../components/Sidebar'),
+  { ssr: false }
+)
 
 export default function Home() {
   return (
@@ -9,8 +15,7 @@ export default function Home() {
         <meta name="description" content="Whatsapp Web 2" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Sidebar/>
-      
+      <SidebarNoSSR style={{width : "30%"}}/>
     </div>
   )
 }
